edc69eef7726c672af03bdbbc793037c96dc23bc
anirudhsardiwal <141425043+anirudhsardiwal@users.noreply.github.com>
2025-01-23 09:51:46 +0000

    kenjee start

---
+++ App-with-KenJee.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.graph_objects as go
+import plotly.express as px
+from datetime import datetime
+
+
+df_agg = pd.read_csv('/kenjee yt data/Aggregated_Metrics_By_Video.csv')
+
