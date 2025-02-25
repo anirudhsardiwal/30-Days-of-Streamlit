778199edf94b6055559346d8b0942a3eb16fa754
anirudhsardiwal <141425043+anirudhsardiwal@users.noreply.github.com>
2025-02-25 08:10:18 +0000

    before changing sql to python

--- dashboard_fanilo.py
+++ dashboard_fanilo.py
@@ -8,17 +8,18 @@ import random
 st.set_page_config(
     page_title="Dashboard Fanilo", page_icon=":bar_chart:", layout="wide"
 )
-with st.sidebar:
-    uploaded_file = st.file_uploader("Choose a file")
+# with st.sidebar:
+#     uploaded_file = st.file_uploader("Choose a file")
 
-if uploaded_file is None:
-    st.info("Please upload a file")
-    st.stop()
+# if uploaded_file is None:
+#     st.info("Please upload a file")
+#     st.stop()
 
-df = pd.read_excel(uploaded_file)
+url = "https://raw.githubusercontent.com/anirudhsardiwal/30-Days-of-Streamlit/main/Financial%20Data%20Clean.xlsx"
+df = pd.read_excel(url)
 
-with st.expander("Show dataframe"):
-    st.dataframe(df, column_config={"Year": st.column_config.NumberColumn(format="%d")})
+# with st.expander("Show dataframe"):
+#     st.dataframe(df, column_config={"Year": st.column_config.NumberColumn(format="%d")})
 
 all_months = [
     "Jan",
@@ -92,16 +93,22 @@ def plot_gauge(
 def plot_top_right():
     sales_data = duckdb.sql(
         f"""
-        with sales_data AS (unpivot (select scenario, business_unit,{",".join(all_months)} from df where Year='2023' and Account='Sales')
-        on {",".join(all_months)} into name month value sales
+        WITH sales_data AS (
+            UNPIVOT (
+                SELECT Scenario, business_unit, {",".join(all_months)} 
+                FROM df 
+                WHERE Year='2023' AND Account='Sales'
+            )
+            ON {",".join(all_months)} 
+            INTO name month VALUE sales
+        ),
+        aggregated_sales AS (
+            SELECT Scenario, business_unit, SUM(sales) AS sales
+            FROM sales_data 
+            GROUP BY Scenario, business_unit
         )
-    ),
-    
-    aggregated_sales as ( select scenario, business_unit, sum(sales) as sales
-    from sales_data group by scenario, business_unit
-    )
-    select * from aggregated_sales
-    """
+        SELECT * FROM aggregated_sales
+        """
     ).df()
 
     fig = px.bar(
@@ -178,11 +185,39 @@ def plot_bottom_right():
     ######################################
 
 
-top_left, top_right = st.columns((2, 1))
-bot_left, bot_right = st.columns(2)
+top_left_col, top_right_col = st.columns((2, 1))
+bot_left_col, bot_right_col = st.columns(2)
 
-with top_left:
+with top_left_col:
     col_1, col_2, col_3, col_4 = st.columns(4)
 
     with col_1:
-        plot_metric
+        plot_metric(
+            "Total Accounts Receivable",
+            6621280,
+            prefix="$",
+            suffix="",
+            show_graph=True,
+            color_graph="rgba(0, 104, 201, 0.2)",
+        )
+
+        plot_gauge(10, "#FF8700", " days", "In Stock", 31)
+
+    with col_2:
+        plot_metric(
+            "Total Accounts Payable", 1630270, "$", "", True, "rgba(255, 43, 43, 0.2)"
+        )
+
+        plot_gauge(1.86, "#0068C9", "%", "Current Ratio", 3)
+
+    with col_3:
+        plot_metric("Equity Ratio", 75.38, "", "%", False, "#d1f2eb")
+
+        plot_gauge(7, "#FF2B2B", " days", "Out Stock", 31)
+
+    with col_4:
+        plot_metric("Debt Equity", 1.10, prefix="", suffix=" %", show_graph=False)
+        plot_gauge(28, "#29B09D", " days", "Delay", 31)
+
+with top_right_col:
+    plot_top_right
